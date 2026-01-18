2dbf816b93d4ac4f56101f7c4212499331e17a97
BABBITTRON\owenh <owen.harbert2@gmail.com>
2026-01-18 05:07:39 +0200

    add paste

--- UIUtils.cs
+++ UIUtils.cs
@@ -8,5 +8,10 @@ namespace BabbittsUnityUtils
         {
             GUIUtility.systemCopyBuffer = text;
         }
+
+        public static string PasteFromClipboard()
+        {
+            return GUIUtility.systemCopyBuffer;
+        }
     }
 }
